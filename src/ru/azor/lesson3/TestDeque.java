f5a58f643575e8166dcd9cc64bed83bde0f1cab2
AZor1973 <azor@mail.ru>
2021-10-11 09:50:30 +0300

    lesson3

---
+++ src/ru/azor/lesson3/TestDeque.java
@@ -0,0 +1,133 @@
+package ru.azor.lesson3;
+
+public class TestDeque {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new DequeImpl<>(5);
+        testRandom(deque);
+        testCirculationDequeLeft(deque);
+        testCirculationDequeRight(deque);
+    }
+
+    private static void testRandom(Deque<Integer> deque) {
+        System.out.println("-----------------------------------");
+        System.out.println("Random test");
+        System.out.println("Clearing the queue: " + deque.removeAll());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(34));
+        System.out.println("Right adding an element: " + deque.insertRight(12));
+        System.out.println("Right adding an element: " + deque.insertRight(20));
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(16));
+        System.out.println("Left adding an element: " + deque.insertLeft(14));
+        System.out.println("Left adding an element: " + deque.insertLeft(17));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("-----------------------------------");
+    }
+
+    private static void testCirculationDequeRight(Deque<Integer> deque) {
+        System.out.println("-----------------------------------");
+        System.out.println("Check the circulation of the deque to the right");
+        System.out.println("Clearing the queue: " + deque.removeAll());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(16));
+        System.out.println("Left adding an element: " + deque.insertLeft(14));
+        System.out.println("Left adding an element: " + deque.insertLeft(29));
+        System.out.println("Left adding an element: " + deque.insertLeft(36));
+        System.out.println("Left adding an element: " + deque.insertLeft(34));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(17));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(18));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(19));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(20));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(21));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(22));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(23));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(24));
+        deque.display();
+        System.out.println("Right deletion of an element: " + deque.removeRight());
+        deque.display();
+        System.out.println("Left adding an element: " + deque.insertLeft(25));
+        deque.display();
+        System.out.println("-----------------------------------");
+    }
+
+    private static void testCirculationDequeLeft(Deque<Integer> deque) {
+        System.out.println("-----------------------------------");
+        System.out.println("Check the circulation of the deque to the left");
+        System.out.println("Clearing the queue: " + deque.removeAll());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(16));
+        System.out.println("Right adding an element: " + deque.insertRight(14));
+        System.out.println("Right adding an element: " + deque.insertRight(29));
+        System.out.println("Right adding an element: " + deque.insertRight(36));
+        System.out.println("Right adding an element: " + deque.insertRight(34));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(17));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(18));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(19));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(20));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(21));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(22));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(23));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(24));
+        deque.display();
+        System.out.println("Left deletion of an element: " + deque.removeLeft());
+        deque.display();
+        System.out.println("Right adding an element: " + deque.insertRight(25));
+        deque.display();
+        System.out.println("-----------------------------------");
+    }
+}
+
